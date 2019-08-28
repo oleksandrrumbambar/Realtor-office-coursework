3caed46dddf75a80c3136927fa4dfb87083fdaec
Tereschenko <Sashapena1337@gmail.com>
2019-08-28 18:39:33 +0300

    Getting started on migration

---
+++ Realtor-office-coursework/DAL/Entities/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    [Table("Accounts")]
+    public class Account
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Password { get; set; }
+
+        [Required]
+        public bool Admin { get; set; }
+
+    }
+}
